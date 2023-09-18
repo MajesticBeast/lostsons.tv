ed538d2b406014614da06256b244f3b1485d85a7
majesticbeast <eric.d.barnard@outlook.com>
2023-09-17 21:28:15 -0700

    add db initialization and helpful comments

--- main.go
+++ main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+	dbConnStr := os.Getenv("DBCONNSTR")
+
+	// Initialize the database connection
+	store, err := NewPostgresStore(dbConnStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Initialize the router and routes
 	r := chi.NewRouter()
 	r.Get("/", handleIndex)
 	r.Get("/health", handleHealth)
+
+	// Start the server
 	http.ListenAndServe(":3000", r)
 }
 
@@ -23,7 +42,7 @@ func handleHealth(w http.ResponseWriter, r *http.Request) {
 	responseWithJSON(w, http.StatusOK, map[string]string{"message": "alive"})
 }
 
-// --> /
+// --> /index
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	responseWithJSON(w, http.StatusOK, map[string]string{"message": "hello world"})
 }
