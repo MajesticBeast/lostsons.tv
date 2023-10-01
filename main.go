f0d886e7600664a7a0d439e8f64ac5ef87e266ee
majesticbeast <eric.d.barnard@outlook.com>
2023-10-01 12:11:07 -0700

    add: logger usage and clarifying comments

--- main.go
+++ main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/majesticbeast/lostsons.tv/logger"
 )
 
 func main() {
+	// Initialize logger
+	log := &logger.StdLogger{}
+
 	godotenv.Load()
 	dbConnStr := os.Getenv("DBCONNSTR")
 
 	// Initialize the database connection
 	store, err := NewPostgresStore(dbConnStr)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
 	}
 
+	// Initialize the database
 	if err := store.Init(); err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
 	}
 
+	// Initialize and run the API server
 	server := NewAPIServer(store)
 	server.Run()
 }
